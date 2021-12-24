10f2e898d0d6759503e6bafc6ced1a61e6f9277d
unknown <thanvanthvinod@gmail.com>
2021-12-25 00:20:53 +0530

    small changes

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 from matplotlib.widgets import Slider
-sns.set()
+try:
+    import seaborn as sns
+except: print('Seaborn not installed')
+else:
+    import seaborn as sns
+    sns.set()
 
 fig, ax = plt.subplots()
 plt.subplots_adjust(bottom=0.35)
@@ -17,24 +21,23 @@ t=np.linspace(0,30,5000)
 #x=Acos(ωt+φ) -> v=-Aωsin(ωt+φ) -> a=-Aω^2cos(ωt+φ)
 #a=-x*ω^2
 
-#angular_frequency 
 plt.title(u"""Simple Harmonic Oscillator
-x(t)=Acos(\u03C9t+\u03C6)e\u207B\u1d48\u1D57""",fontname = 'calibri')
+x(t)=Ae\u207B\u1d48\u1D57cos(\u03C9t+\u03C6)""",fontname = 'calibri')
 phase=np.deg2rad(0)
 amplitude=1
 omega=1
 x=[amplitude*np.cos((omega*i)+phase) for i in t]
 line,=plt.plot(t,x)
-axfreq = plt.axes([0.3, 0.1, 0.65, 0.03])
-axamplitude = plt.axes([0.3, 0.2, 0.65, 0.03])
-axdampingcoefficient = plt.axes([0.3, 0.05, 0.65, 0.03])
-axphase = plt.axes([0.3, 0.15, 0.65, 0.03])
+axfreq = plt.axes([0.275, 0.15, 0.625, 0.03])
+axamplitude = plt.axes([0.275, 0.2, 0.625, 0.03])
+axdampingcoefficient = plt.axes([0.275, 0.05, 0.625, 0.03])
+axphase = plt.axes([0.275, 0.1, 0.625, 0.03])
  
 omega = Slider(axfreq, 'Frequency - \u03C9(rad/s)', 0.0, 20.0, 1)
  
 amplitude = Slider(axamplitude, 'Amplitude - a (m)', 0.0, 10.0, 1)
 
-damping = Slider(axdampingcoefficient, u'Damping coefficient - d', 0.0,1,0)
+damping = Slider(axdampingcoefficient, u'Damping coeff - d', 0.0,1,0)
 
 phase = Slider(axphase, 'phase - \u03C6 (deg)', 0.0,90,0)
 
